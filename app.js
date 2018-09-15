108b5fee110899d825e42e5b268c64a5ac1401d1
PulsarFox <samy.vincentffs@gmail.com>
2018-09-16 00:26:05 +0200

    Add routes and header

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hbs = require('hbs');
 
 var app = express();
 
@@ -32,10 +32,11 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+hbs.registerPartials(__dirname + '/views/partials');
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var Handlebars = require('handlebars');
+
+Handlebars.registerPartial('header', '{{header}}')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Adrasamen' });
 });
+
+router.get('/Contribute', function(req, res, next){
+  res.render('contribute', {title: 'Contribuez'})
+})
+
+router.get('/Contact', function(req, res, next){
+  res.render('contact', {title: 'Contactez-nous'})
+})
+
+router.get('/Team', function(req, res, next){
+  res.render('team', {title: 'La team'})
+})
 
 module.exports = router;
